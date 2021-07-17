38e46de496e93fa318abbab6157156051144f4e5
evgennn32 <evgennn32@mail.ru>
2021-07-17 11:54:40 +0300

    feat: add infinity scroll

--- src/components/adminCategories/adminCategories.ts
+++ src/components/adminCategories/adminCategories.ts
@@ -17,7 +17,7 @@ export default class AdminCategories extends Component {
     this.app = app;
     this.html = '';
     this.loadLimit = 3;
-    this.lastLoadedCategory = 5
+    this.lastLoadedCategory = 8;
   }
 
   render(): void {
@@ -25,37 +25,47 @@ export default class AdminCategories extends Component {
     const addNewCategoryCard = new AddNewCategoryCard(this.app);
     addNewCategoryCard.render()
     this.element.append(addNewCategoryCard.element);
-    const firstLoadLimit = 5;
-    try {
-      const categories = this.app.apiService.getCategories();
-      categories.then((allCategories) => {
-        allCategories.forEach((category:{name:string, categoryId: number}) => {
-          if(category.name){
-            const wordsNumberQuery = this.app.apiService.getCategoryWordsNumber(category.categoryId);
-            wordsNumberQuery.then(wordsNumber => {
-              const categoryCard = new AdminCategoryCard(this.app, category, wordsNumber);
-              categoryCard.render();
-              this.element.append(categoryCard.element);
-            });
-          }
-        });
-      });
-
-    } catch (e) {
-      throw Error(e)
-    }
-
-
-
-
+    const firstLoadLimit = 8;
+    this.renderCategories(firstLoadLimit);
+    this.addInfinityScroll();
   }
 
   buildHtml(): string {
-
     return this.html;
   }
 
+  renderCategories(limit = 0, last = 0): void {
+    const categories = this.app.apiService.getCategories(limit, last);
+    categories.then((allCategories) => {
+      allCategories.forEach((category: { name: string, categoryId: number }) => {
+        if (category.name) {
+          const wordsNumberQuery = this.app.apiService.getCategoryWordsNumber(category.categoryId);
+          wordsNumberQuery.then(wordsNumber => {
+            const categoryCard = new AdminCategoryCard(this.app, category, wordsNumber);
+            categoryCard.render();
+            this.element.append(categoryCard.element);
+          });
+        }
+      });
+    }).catch((e) => {
+      throw Error(e)
+    });
+  }
 
+  addInfinityScroll(): void {
+    window.addEventListener('scroll', () => {
+      const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+      } = document.documentElement;
 
-
+      if (scrollTop + clientHeight >= scrollHeight - 5) {
+        this.renderCategories(this.loadLimit, this.lastLoadedCategory);
+        this.lastLoadedCategory += this.loadLimit;
+      }
+    }, {
+      passive: true
+    });
+  }
 }
--- src/components/adminWords/adminWords.ts
+++ src/components/adminWords/adminWords.ts
@@ -7,10 +7,20 @@ import AddNewWordCard from "../adminWordCard/addNewWordCard";
 export default class AdminWords extends Component {
   private html: string;
 
+  private loadLimit: number;
+
+  private lastLoadedWords: number;
+
+  private totalWords: number;
+
   constructor(protected app: App) {
     super('div', ['admin-words']);
     this.app = app;
     this.html = '';
+    this.loadLimit = 3;
+    this.lastLoadedWords = 5;
+    this.totalWords = 0;
+    this.updateTotalWords();
   }
 
   render(): void {
@@ -18,8 +28,18 @@ export default class AdminWords extends Component {
     const addNewWordCard = new AddNewWordCard(this.app);
     addNewWordCard.render();
     this.element.append(addNewWordCard.element);
+    const firstRenderLimit = 5;
+    this.renderWords(firstRenderLimit);
+    this.addInfinityScroll();
+  }
+
+  buildHtml(): string {
+    return this.html;
+  }
+
+  renderWords(limit= 0, last = 0): void {
     try {
-      const words = this.app.apiService.getWords(this.app.appData.adminWordsCategory);
+      const words = this.app.apiService.getWords(this.app.appData.adminWordsCategory,limit, last);
       words.then((allWords) => {
         allWords.forEach(
           (word: {
@@ -34,7 +54,6 @@ export default class AdminWords extends Component {
               const categoryCard = new AdminWordCard(this.app, word);
               categoryCard.render();
               this.element.append(categoryCard.element);
-
             }
           });
       });
@@ -42,14 +61,28 @@ export default class AdminWords extends Component {
     } catch (e) {
       throw Error(e)
     }
-
-
   }
 
-  buildHtml(): string {
-
-    return this.html;
+  updateTotalWords(): void {
+    this.app.apiService.getCategoryWordsNumber(this.app.appData.adminWordsCategory).then((wordsNumber => {
+      this.totalWords = wordsNumber;
+    }));
   }
 
+  addInfinityScroll(): void {
+    window.addEventListener('scroll', () => {
+      const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+      } = document.documentElement;
 
+      if (scrollTop + clientHeight >= scrollHeight - 5 && this.lastLoadedWords <= this.totalWords) {
+        this.renderWords(this.loadLimit, this.lastLoadedWords);
+        this.lastLoadedWords += this.loadLimit;
+      }
+    }, {
+      passive: true
+    });
+  }
 }
